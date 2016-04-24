2ad9068c4b7191bf7a9e12d391f94e54fd61f5d7
JCubz <jaocbs.emmett@gmail.com>
2016-04-23 18:13:47 -0700

    update db handler

--- csv_parser/DatabaseHandle.py
+++ csv_parser/DatabaseHandle.py
@@ -12,15 +12,18 @@ conn = MySQLdb.connect(host="192.254.148.221",
                        user="bayes",
                        passwd="Qn4Jy2WodGYekwjskICV",
                        db="bayes")
-cursor = conn.cursor()  # except cursur.errorhandler, e:
 
-
-#     print "error %d: %s" % (e.args[0], e.args[1])
-#     sys.exit(1)
 
 def execute_commit(query):
     print query
-    cursor.execute(query);
+    cursor = conn.cursor()
+
+    if isinstance(query, basestring):
+        cursor.execute(query)
+    else:
+        for q in query:
+            cursor.execute(q)
+
     for row in cursor.fetchall():
         for i in row:
             print i,
@@ -28,13 +31,41 @@ def execute_commit(query):
     cursor.close()
 
 
-def check_table_exists(tableName):
-    print "checkingTable"
+def check_table_exists(table_name):
+    dbcur = conn.cursor()
+    conn.execute("""
+        SELECT COUNT(*)
+        FROM information_schema.tables
+        WHERE table_name = '{0}'
+        """.format(table_name.replace('\'', '\'\'')))
+    if dbcur.fetchone()[0] == 1:
+        dbcur.close()
+        print "Table " + table_name + " Exists"
+        return True
 
-# def main():
-# select_statment = "SELECT * FROM `bayes`.`test`;"
-# return execute_query(select_statment)
+    dbcur.close()
+    "Table " + table_name + "does not Exist"
+    return False
 
+def check_field_exists(column_name, table_name):
+    dbcur = conn.cursor()
 
-if __name__ == "__main__":
-    print "executed"
+    #conn.execute("""
+    #    SHOW columns
+    #    FROM " + table_name +
+    #    "WHERE table_name = '{0}'
+    #    """.format(table_name.replace('\'', '\'\'')))
+    if dbcur.fetchone()[0] == 1:
+        dbcur.close()
+        print "Table " + table_name + " Exists"
+        return True
+
+    dbcur.close()
+    # "Table " + table_name + "does not Exist"
+    # return False
+    # SHOW
+    # columns
+    # from
+    # `yourtable`
+    # where
+    # field = 'yourfield'
